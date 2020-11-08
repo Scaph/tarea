0d0ed6e6946bcff38af9f7e75eb500b9ff40fc77
José Eduardo Rivera Gámez <jose.riveragm@uanl.edu.mx>
2020-11-07 20:47:38 -0600

    Script chiquito de nmap

---
+++ nmapscr.sh
@@ -0,0 +1,8 @@
+hostname -I
+nmap 192.168.0.*
+curl ifconfig.me
+nmap --script=vuln 187.188.73.61
+nmap --script default 192.168.0.1
+nmap 187.188.73.61
+curl https://tools.keycdn.com/geo.json?host=187.188.73.61
+
